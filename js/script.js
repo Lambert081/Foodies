a07b31941b7f0e047c6e41bc79c1cfb19baee1d2
uzoma lambert <lambertuzoma25@gmail.com>
2025-01-18 12:51:16 +0100

    add modify

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-let hamburgerElem = document.getElementById("hamburger");
-let navElem = document.getElementById("mobile-menu");
-
-hamburgerElem.addEventListener("click", function()
-{
-    navElem.classList.toggle("active")
-    hamburgerElem.classList.toggle("fa-times");
-})
+ 
